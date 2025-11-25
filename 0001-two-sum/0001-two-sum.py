ef38e6dd750b97acaad004006f0e4ddd15ada79e
Farzan <67397870+farzan-dehbashi@users.noreply.github.com>
2025-11-24 20:29:28 -0500

    Time: 0 ms (100%), Space: 18.7 MB (66.37%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = set()
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return nums.index(target-num), i
+            seen.add(num)
+        return False
+        
