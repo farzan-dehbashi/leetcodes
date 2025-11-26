65800eda25dbcb3210b70a355f9a43b93ea81f9a
Farzan <67397870+farzan-dehbashi@users.noreply.github.com>
2025-11-25 22:00:30 -0500

    Time: 74 ms (69.83%), Space: 18.1 MB (23.28%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,10 +1,12 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        tower = [[0.0] * (r+1) for r in range(query_row+1)]
-        tower[0][0]=poured
-        for r in range(query_row):
+        tower = [[0.0] * (r+1) for r in range(query_row+2)]
+        tower[0][0] = poured
+
+        for r in range(query_row+1):
             for c in range(r+1):
                 if tower[r][c] > 1:
                     overflow = (tower[r][c] - 1) / 2
-                    tower[r+1][c], tower[r+1][c+1] =  tower[r+1][c] + overflow, tower[r+1][c+1]+overflow
+                    tower[r+1][c] += overflow
+                    tower[r+1][c+1] += overflow
         return min(1.0, tower[query_row][query_glass])
