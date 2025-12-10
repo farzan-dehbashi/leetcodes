02acffc2e2f46de3cea12c7d32ce4c31944398b4
Farzan <67397870+farzan-dehbashi@users.noreply.github.com>
2025-12-10 15:59:54 -0500

    Time: 11 ms (36.39%), Space: 23.1 MB (19.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,7 +3,7 @@ class Solution:
         s = [x.lower() for x in s if x.isalnum()]
         l, r = 0, len(s) - 1
         while l<=r:
-            if not s[l] == s[r]:
+            if s[l] != s[r]:
                 return False
             l, r = l+1, r-1
         return True
