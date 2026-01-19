3524c6636027c47a909d28eb713d8cbb93421baf
Farzan <67397870+farzan-dehbashi@users.noreply.github.com>
2026-01-19 13:23:56 -0500

    Time: 18 ms (36.94%), Space: 24.1 MB (6.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,13 @@
 class Solution:
     def groupAnagrams(self, words: List[str]) -> List[List[str]]:
         angs = collections.defaultdict(list)
+
         for word in words:
             pattern = [0] * 26
             for char in word:
                 pattern[ord(char) - ord('a')] += 1
             angs[tuple(pattern)].append(word)
-        return [word for word in angs.values()] 
+        return [angs[x] for x in angs]
 
 
       
