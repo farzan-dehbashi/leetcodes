7bac6b313927e4cd8ba4f45d40dfa34af223cfe6
Farzan <67397870+farzan-dehbashi@users.noreply.github.com>
2026-01-19 21:36:26 -0500

    Time: 0 ms (100%), Space: 19.4 MB (13.51%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,16 +1,19 @@
 class Solution:
     def compress(self, original: List[str]) -> int:
-        p = 0
-        res = []
-        while p < len(original):
-            num, cur = 0, original[p]
+        w = r = 0
+        while r < len(original):
+            cur = original[r]
+            c = 0
 
-            while p < len(original) and original[p] == cur:
-                num, p = num+1, p+1
+            while r < len(original) and original[r] == cur:
+                c, r = c+1, r+1
+            
+            original[w] = cur
+            w += 1
 
-            res.append(cur)
-            if num > 1:
-                for char in str(num):
-                    res.append(char)
-        original[:] = res
+            if c > 1:
+                for digit in str(c):
+                    original[w] = digit
+                    w += 1
+        return w
 
